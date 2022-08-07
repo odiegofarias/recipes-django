0347c80ed0688b86bfda6f5d9f63930cfe970de1
odiegofarias <diego.farias.fernandes@gmail.com>
2022-08-07 20:24:19 -0300

    Mudando os arquivos de testes para pasta recipes/tests e separando de acordo com o contexto

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RecipeURLsTest(TestCase):
+    def test_se_a_home_url_de_recipe_esta_correta(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_se_a_category_url_de_recipe_esta_correta(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_se_a_recipe_detalhe_url_de_recipe_esta_correta(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -3,20 +3,6 @@ from django.urls import reverse, resolve
 from recipes import views
 
 
-class RecipeURLsTest(TestCase):
-    def test_se_a_home_url_de_recipe_esta_correta(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_se_a_category_url_de_recipe_esta_correta(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_se_a_recipe_detalhe_url_de_recipe_esta_correta(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
-
-
 class RecipeViewsTest(TestCase):
     def test_se_a_view_home_de_recipe_esta_correta(self):
         #  resolve: qual função está sendo usada por uma URL
