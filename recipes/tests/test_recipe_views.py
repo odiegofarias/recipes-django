18df8d9fca75139ffb8db85a1ea7eac4a3cafc9f
odiegofarias <diego.farias.fernandes@gmail.com>
2022-08-07 21:29:24 -0300

    Adicionando testes status code 404 à categoria, detalhes e home  e 200 à home

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -31,3 +31,36 @@ class RecipeViewsTest(TestCase):
             )
         )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_retorna_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_category_view_retorna_status_code_404_se_nao_encontrou_recipes(self): # noqa: 501
+        response = self.client.get(
+            reverse(
+                'recipes:category',
+                kwargs={'category_id': 999}
+            )
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_recipe_detalhe_view_retorna_status_code_404_se_nao_encontrou_recipe(self): # noqa: 501
+        response = self.client.get(
+            reverse(
+                'recipes:recipe',
+                kwargs={'id': 999}
+            )
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_home_view_carrega_template_correto(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_mostra_no_recipe_founds_se_nao_tem_receita(self): # noqa: 501
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'Nenhuma receita para mostrar',
+            response.content.decode('utf-8'),
+        )
