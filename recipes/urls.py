0bd460cbd4f54a04dabb9994ac9cf4de7e3931fc
odiegofarias <diego.farias.fernandes@gmail.com>
2022-09-15 21:12:55 -0300

    Criando a url e view de API retornando JSON com a lista de recipes e os detalhes da recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,11 +5,34 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.RecipeListViewHome.as_view(), name='home'),
-    path('recipes/search/', views.RecipeListViewSearch.as_view(), name='search'),  # noqa: E501
-    path
-    (
-        'recipes/category/<int:category_id>/', views.RecipeListViewCategory.as_view(), name='category'  # noqa: E501
+    path(
+        '',
+        views.RecipeListViewHome.as_view(),
+        name='home',
     ),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name='recipe'),
+    path(
+        'recipes/search/',
+        views.RecipeListViewSearch.as_view(),
+        name='search',
+    ),
+    path(
+        'recipes/category/<int:category_id>/',
+        views.RecipeListViewCategory.as_view(),
+        name='category',
+    ),
+    path(
+        'recipes/<int:pk>/',
+        views.RecipeDetail.as_view(),
+        name='recipe',
+    ),
+    path(
+        'recipes/api/v1',
+        views.RecipeListViewHomeApi.as_view(),
+        name='recipes_api_vi',
+    ),
+    path(
+        'recipes/api/v1/<int:pk>',
+        views.RecipeDetailApi.as_view(),
+        name='recipes_api_v1_detail'
+    )
 ]
