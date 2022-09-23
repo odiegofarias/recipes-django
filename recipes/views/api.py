0e9d3c8ecfeb906947b7d19eaa17fe81b0b9f053
odiegofarias <diego.farias.fernandes@gmail.com>
2022-09-22 21:46:45 -0300

    Substuindo APIView da RecipeAPIv2List para ListCreateAPIView e incluindo paginação na resposta

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,41 +1,49 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..models import Recipe
-from ..serializers import RecipeSerializer
+from ..serializers import RecipeSerializer, TagSerializer
 from django.shortcuts import get_object_or_404
 from tag.models import Tag
-from ..serializers import TagSerializer
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 
 
-class RecipeAPIv2List(APIView):
-    def get(self, request):
-        recipes = Recipe.objects.get_published()[:10]
-        #  Sempre que mandar uma queryset, precisa colocar "many=True"
-        serializer = RecipeSerializer(
-            instance=recipes,
-            many=True,
-            context={'request': request}
-        )
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 10
 
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data,
-            context={'request': request},
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save(
-            author_id=1, category_id=1,
-            tags=[1, 2],
-        )
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
+    # def get(self, request):
+    #     recipes = Recipe.objects.get_published()[:10]
+    #     #  Sempre que mandar uma queryset, precisa colocar "many=True"
+    #     serializer = RecipeSerializer(
+    #         instance=recipes,
+    #         many=True,
+    #         context={'request': request}
+    #     )
 
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-        )
+    #     return Response(serializer.data)
+
+    # def post(self, request):
+    #     serializer = RecipeSerializer(
+    #         data=request.data,
+    #         context={'request': request},
+    #     )
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save(
+    #         author_id=1, category_id=1,
+    #         tags=[1, 2],
+    #     )
+
+    #     return Response(
+    #         serializer.data,
+    #         status=status.HTTP_201_CREATED
+    #     )
 
 
 class RecipeApiv2Detail(APIView):
