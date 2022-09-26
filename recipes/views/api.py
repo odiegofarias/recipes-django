a4888e5380a008ff99cb53eceab147ba862d0625
odiegofarias <diego.farias.fernandes@gmail.com>
2022-09-26 20:22:58 -0300

    Retiradas as GenericViews de Métodos HTTP e colocando as duas num Viewset para evitar repetição de código.

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -4,23 +4,19 @@ from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 from django.shortcuts import get_object_or_404
 from tag.models import Tag
+
 from rest_framework.generics import (
     ListCreateAPIView, RetrieveUpdateDestroyAPIView
 )
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
 
 class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 10
 
 
-class RecipeAPIv2List(ListCreateAPIView):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
-
-
-class RecipeApiv2Detail(RetrieveUpdateDestroyAPIView):
+class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
